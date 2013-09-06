759a1009220d224452c341cc3ab8dd111a335a95
Jade McGough <jade@thezets.com>
2013-09-06 00:19:27 -0700

    oops I ignored all of config/ :(

---
+++ config/application.rb
@@ -0,0 +1,50 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+# load configuration variabales
+ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
+
+#mailer config
+#Rails::Application.configure do
+#  config.action_mailer.smtp_settings = {
+#      :address   => "smtp.mandrillapp.com",
+#      :port      => '587', # ports 587 and 2525 are also supported with STARTTLS
+#      :enable_starttls_auto => true, # detects and uses STARTTLS
+#      :user_name => ENV['mandrill_username'],
+#      :password  => ENV['mandrill_apikey'], # SMTP password is any valid API key
+#      :authentication => 'plain', # Mandrill supports 'plain' or 'login'
+#      :domain => 'heroku.com', # your domain to identify your server when connecting
+#  }
+#end
+
+module ClStalker
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    config.action_mailer.smtp_settings = {
+        :address   => "smtp.mandrillapp.com",
+        :port      => '587', # ports 587 and 2525 are also supported with STARTTLS
+        :enable_starttls_auto => true, # detects and uses STARTTLS
+        :user_name => ENV['mandrill_username'],
+        :password  => ENV['mandrill_apikey'], # SMTP password is any valid API key
+        :authentication => 'plain', # Mandrill supports 'plain' or 'login'
+        :domain => 'heroku.com', # your domain to identify your server when connecting
+    }
+
+  end
+end
