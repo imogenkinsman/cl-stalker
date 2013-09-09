7961b48840b25f93500cfd4c32c97e77c502c578
Jade McGough <jade@thezets.com>
2013-09-09 04:21:09 -0700

    reorganizing config files

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-# load configuration variabales
-ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
-
 module ClStalker
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,16 +19,6 @@ module ClStalker
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    config.action_mailer.smtp_settings = {
-        :address   => "smtp.mandrillapp.com",
-        :port      => '587', # ports 587 and 2525 are also supported with STARTTLS
-        :enable_starttls_auto => true, # detects and uses STARTTLS
-        :user_name => ENV['MANDRILL_USERNAME'],
-        :password  => ENV['MANDRILL_APIKEY'], # SMTP password is any valid API key
-        :authentication => 'plain', # Mandrill supports 'plain' or 'login'
-        :domain => 'heroku.com', # your domain to identify your server when connecting
-    }
 
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ ClStalker::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # load configuration vars locally for development
+  ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
+
 end
